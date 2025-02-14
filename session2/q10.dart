0a4ac7249db7fedee05431e1e84c951fa320fcef
HussHE <husshe94@gmail.com>
2025-02-14 15:45:24 +0200

    forget 1 asnwer

---
+++ session2/q10.dart
@@ -0,0 +1,12 @@
+// Using Comparison Operators
+// Objective: Use comparison operators to compare values.
+// Instructions:
+// - Create two integer variables, x and y, with values 50 and 30 respectively.
+// - Print true if x is less than y, otherwise print false.
+
+void main(List<String> args) {
+  int x = 50;
+  int y = 30;
+
+  x < y ? print("true") : print("false");
+}
