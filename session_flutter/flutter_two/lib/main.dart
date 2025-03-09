874ade4c6124d04df829efadd97bac716413f0d8
HussHE <husshe94@gmail.com>
2025-03-09 20:48:55 +0200

    flutter_homework_two

--- session_flutter/flutter_two/lib/main.dart
+++ session_flutter/flutter_two/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_one/homepage.dart';
+import 'package:flutter_two/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+
+      home: const Homepage(),
     );
   }
 }
